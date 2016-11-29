c0096f253b1a633edd591dc2ec57e570fc9fdec7
kk8686 <86868057@qq.com>
2016-11-29 16:42:33 +0800

    整理URL的配置
    Signed-off-by: kk8686 <86868057@qq.com>

--- server/home/config/config.php
+++ server/home/config/config.php
@@ -19,33 +19,7 @@ $config = [
             'errorAction' => 'site/error',
         ],
         'db' => include(PROJECT_PATH . '/server/common/config/db.php'),
-        'urlManager' => [
-            'enablePrettyUrl' => true,
-            'showScriptName' => false,
-            'rules' => [
-				//工作者
-				'worker/register.do' => 'worker/register',
-				'worker/login.do' => 'worker/login',
-				'worker/logout.do' => 'worker/logout',
-				'worker/check-login.do' => 'worker/check-login',
-				'worker/headbar.json' => 'worker/headbar',
-				
-				//项目
-				'project/list.json' => 'project/list',
-				'project/<projectId:\d+>.json' => 'project/desc',
-				'project/<projectId:\d+>/members.json' => 'project/members',
-				'project/add.do' => 'project/add',
-				'project/invite-member.do' => 'project/invite-member',
-				
-				//任务
-				'task/add.do' => 'task/add',
-				'task/<categoryId>/tasks.json' => 'task/list',
-				'project/<projectId:\d+>/task-categorys.json' => 'task/task-categories',
-				'task/move.do' => 'task/move',
-				
-				'<page:.+>.htm<xx:l{0,1}>' => 'site/r',
-            ],
-        ],
+        'urlManager' => include(__DIR__ . '/url.php'),
     ],
 	'on beforeAction' => function($event){
 		//暂时在此做统一登陆校验
