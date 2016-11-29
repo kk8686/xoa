562e524fdc693dac19c98ecd2b48fafb9dfa2b62
kk8686 <86868057@qq.com>
2016-11-29 16:43:17 +0800

    补充注释
    Signed-off-by: kk8686 <86868057@qq.com>

--- server/tests/codeception/_support/Helper/Unit.php
+++ server/tests/codeception/_support/Helper/Unit.php
@@ -15,14 +15,31 @@ class Unit extends \Codeception\Module
         $this->_tester = $tester;
     }
 	
-	public function assertHasKeys($keys, $array, $only = true, $message = ''){
+	/**
+	 * 断言数组包含指定的key集合
+	 * @author KK
+	 * @param array $keys 要断言包含的key集合
+	 * @param array $array 被断言的数组
+	 * @param bool $only 是否只允许包含这些，false的话则$array多出其它key也可以通过
+	 * @param string $message 断言失败提示消息
+	 */
+	public function assertHasKeys(array $keys, array $array, bool $only = true, string $message = ''){
 		foreach($keys as $key){
 			PHPUnit_Framework_Assert::assertArrayHasKey($key, $array, $message);
 		}
-		PHPUnit_Framework_Assert::assertEmpty(array_diff($keys, array_keys($array)), '数组有多出的key');
+		$only && PHPUnit_Framework_Assert::assertEmpty(array_diff($keys, array_keys($array)), '数组有多出的key');
 	}
 	
-	public function assertListHasKeys($keys, $list, $only = true, $message = ''){
+	
+	/**
+	 * 断言阵列数组包含指定的key集合
+	 * @author KK
+	 * @param array $keys 要断言包含的key集合
+	 * @param array $list 被断言的阵列数组
+	 * @param bool $only 是否只允许包含这些，false的话则$list里的每一个数组多出其它key也可以通过
+	 * @param string $message 断言失败提示消息
+	 */
+	public function assertListHasKeys(array $keys, array $list, bool $only = true, string $message = ''){
 		foreach($list as $item){
 			$this->assertHasKeys($keys, $item, $only, $message);
 		}
