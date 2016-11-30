dd5ef849bbbc4380f598c4c9e081a648dda96c3b
kk8686 <86868057@qq.com>
2016-11-30 22:43:59 +0800

    补充类的注释
    Signed-off-by: KK <86868057@qq.com>

--- server/tests/codeception/_support/models/ArrayFieldModel.php
+++ server/tests/codeception/_support/models/ArrayFieldModel.php
@@ -3,6 +3,11 @@ namespace xoa_test\_support\models;
 
 use xoa\common\ext\behaviors\ArrayField;
 
+/**
+ * 用于测试xoa\common\ext\behaviors\ArrayField的模型
+ * @author KK
+ * @see \xoa_test\unit\yii_ext\ArrayFieldTest
+ */
 class ArrayFieldModel extends \yii\db\ActiveRecord{
 	public static function tableName(){
 		return '_test_array_field';
