8136cb44aaa8393a85ff227ec17366feca9eba66
KK <86868057@qq.com>
2016-12-20 19:08:55 +0800

    优化模拟的任务详情数据

--- frontent/home-mock/task/detail/detail.json.js
+++ frontent/home-mock/task/detail/detail.json.js
@@ -3,8 +3,8 @@ module.exports = dataMocker.mock({
 	code : 0,
 	data : {
 		'id|1-999' : 1,
-		title : 'aaa',
-		detail : 'bbbbbbb',
+		title : /(开发|修复)[\u4E00-\u9FA5]{4,20}功能/,
+		detail : /(开发|修复) [\u4E00-\u9FA5]{15,40} 功能/,
 		'workers|1-6' : [{
 			name : /(小明|小红|陈莹莹|王晶晶|张风|林云龙)/,
 			avatar : /\/data\/worker\/avatar\/xx\.jpg\?id=[1-5]/
